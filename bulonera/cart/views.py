ab604cee26b006c35bf05866ca5238ea7dc0abc2
melafrancom <melafrancom@gmail.com>
2024-11-13 12:03:18 -0300

    arreglado de bugs y errores del carrito

--- bulonera/cart/views.py
+++ bulonera/cart/views.py
@@ -169,7 +169,7 @@ def remove_cart_item(request, product_id, cart_item_id):
         cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
     else:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_item = Cart.objects.get(product=product, cart=cart, id=cart_item_id)
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
         
     cart_item.delete()
     return redirect('cart')
