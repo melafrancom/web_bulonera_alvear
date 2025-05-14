6c64764d45ada0ca8b06f31a7331e00aa4b827ca
melafrancom <melafrancom@gmail.com>
2025-05-14 13:10:25 -0300

    eliminación de imagenes y migraciones

--- bulonera/contact/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.contact_view, name='contact'),
-    path('exito/', views.contact_success, name='contact_success'),
-]
